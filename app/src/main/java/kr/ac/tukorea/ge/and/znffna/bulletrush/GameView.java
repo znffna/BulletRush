4382889cf03681c41a3230a0af01f7f2e2ac4eb7
znffna <znffna@gmail.com>
2025-04-13 18:00:18 +0900

    GameView의 외곽선 및 디버그용 grid 출력

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameView.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameView.java
@@ -2,6 +2,10 @@ package kr.ac.tukorea.ge.and.znffna.bulletrush;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+
+    private static final float SCREEN_WIDTH = 9.0f;
+    private static final float SCREEN_HEIGHT = 16.0f;
+    private final Matrix transformMatrix = new Matrix();
+
+
     public GameView(Context context) {
         super(context);
         init();
@@ -21,10 +31,58 @@ public class GameView extends View {
     }
 
     private void init() {
+        // 실직적 생성자
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        float view_ratio = (float)w / (float)h;
+        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+        transformMatrix.reset();
+        if (view_ratio > game_ratio) {
+            float scale = h / SCREEN_HEIGHT;
+            transformMatrix.preTranslate((w - h * game_ratio) / 2, 0);
+            transformMatrix.preScale(scale, scale);
+        } else {
+            float scale = w / SCREEN_WIDTH;
+            transformMatrix.preTranslate(0, (h - w / game_ratio) / 2);
+            transformMatrix.preScale(scale, scale);
+        }
     }
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+        canvas.setMatrix(transformMatrix);
+        drawDebugBackground(canvas);
+        canvas.drawRect(borderRect, borderPaint);
+    }
+
+    private RectF borderRect;
+    private Paint borderPaint, gridPaint;
+    private void drawDebugBackground(@NonNull Canvas canvas) {
+        if (borderRect == null) {
+            borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+            borderPaint = new Paint();
+            borderPaint.setStyle(Paint.Style.STROKE);
+            borderPaint.setStrokeWidth(0.1f);
+            borderPaint.setColor(Color.RED);
+
+            gridPaint = new Paint();
+            gridPaint.setStyle(Paint.Style.STROKE);
+            gridPaint.setStrokeWidth(0.01f);
+            gridPaint.setColor(Color.GRAY);
+        }
+
+        canvas.drawRect(borderRect, borderPaint);
+        for (float x = 1.0f; x < SCREEN_WIDTH; x += 1.0f) {
+            canvas.drawLine(x, 0, x, SCREEN_HEIGHT, gridPaint);
+        }
+        for (float y = 1.0f; y < SCREEN_HEIGHT; y += 1.0f) {
+            canvas.drawLine(0, y, SCREEN_WIDTH, y, gridPaint);
+        }
     }
 }
