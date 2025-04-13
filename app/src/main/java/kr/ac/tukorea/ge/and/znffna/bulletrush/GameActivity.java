28c52f4ad8cb1bce0c5970fbb90a88fd7861a498
znffna <znffna@gmail.com>
2025-04-13 17:14:55 +0900

    GameActivity 생성

---
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameActivity.java
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.ge.and.znffna.bulletrush;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
