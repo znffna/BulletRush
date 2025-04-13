1cfa15ff8e7fae71511271413abe052f5b1bbb57
znffna <znffna@gmail.com>
2025-04-13 17:51:57 +0900

    GameActivity에서 GameView를 만들고 사용

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.and.znffna.bulletrush;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
+    private GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+
+        setFullScreen();
+    }
+
+    public void setFullScreen() {
+        int flags = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        gameView.setSystemUiVisibility(flags);
     }
 }
