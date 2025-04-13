4487fd96a01bae336cb5ab73ff02f4fe43de00ea
znffna <znffna@gmail.com>
2025-04-13 17:53:50 +0900

    GameView의 Init, onDraw 선언

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameView.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/GameView.java
@@ -1,10 +1,30 @@
 package kr.ac.tukorea.ge.and.znffna.bulletrush;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class GameView extends View {
     public GameView(Context context) {
         super(context);
+        init();
+    }
+
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
