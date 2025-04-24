6a9bec68569d8e178b6f7d41dea8c84d67399c80
znffna <znffna@gmail.com>
2025-04-24 19:43:27 +0900

    Player 생성

---
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/game/Player.java
@@ -0,0 +1,12 @@
+package kr.ac.tukorea.ge.and.znffna.bulletrush.game;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.AnimSprite;
+
+public class Player extends AnimSprite {
+
+    public Player(int mipmapId, float fps) {
+        super(mipmapId, fps);
+    }
+
+
+}
