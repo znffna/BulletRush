a84e553d0411c9123563adef8da6583fef8e2128
znffna <znffna@gmail.com>
2025-05-03 20:05:54 +0900

    Bullet 에 recycle, boxcollide, Layer 인터페이스 삽입

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/game/Bullet.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/game/Bullet.java
@@ -1,20 +1,53 @@
 package kr.ac.tukorea.ge.and.znffna.bulletrush.game;
 
+import android.graphics.RectF;
+
 import kr.ac.tukorea.ge.and.znffna.bulletrush.R;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IBoxCollidable;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.ILayerProvider;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IRecyclable;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
-public class Bullet extends Sprite {
+public class Bullet extends Sprite implements IRecyclable, IBoxCollidable, ILayerProvider<MainScene.Layer> {
     private static final float SPEED = 50f;
     private static final float BULLET_WIDTH = 30f;
     private static final float BULLET_HEIGHT = BULLET_WIDTH;
+    private int power;
 
-    public Bullet(float angle_radian) {
-        super(R.mipmap.bullet);
-        this.dx = (float) (SPEED * Math.cos(angle_radian));
-        this.dy = (float) (SPEED * Math.sin(angle_radian));
-
-        setPosition(100, 100, BULLET_WIDTH, BULLET_HEIGHT);
+    public static Bullet get(float x, float y, float angle, int power) {
+        return Scene.top().getRecyclable(Bullet.class).init(x, y, angle, power);
     }
 
+    private Bullet init(float x, float y, float angle, int power) {
+        setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+        this.dx = (float) (SPEED * Math.cos(angle));
+        this.dy = (float) (SPEED * Math.sin(angle));
+        this.power = power;
+        return this;
+    }
+
+    public Bullet() {
+        super(R.mipmap.bullet);
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    @Override
+    public RectF getCollisionRect() {
+        return dstRect;
+    }
+
+    @Override
+    public MainScene.Layer getLayer() {
+        return MainScene.Layer.bullet;
+    }
+
+    @Override
+    public void onRecycle() {
+
+    }
 }
