a020fb3d49685a534bbcd01367ee24cec8ca919f
znffna <znffna@gmail.com>
2025-06-01 23:52:41 +0900

    Player 체력 출력용 Gauge 추가

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/game/MainScene.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/game/MainScene.java
@@ -1,11 +1,13 @@
 package kr.ac.tukorea.ge.and.znffna.bulletrush.game;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.MotionEvent;
 
 import kr.ac.tukorea.ge.and.znffna.bulletrush.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.JoyStick;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.util.Gauge;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class MainScene extends Scene {
@@ -25,6 +27,7 @@ public class MainScene extends Scene {
 
         joyStick = new JoyStick(R.mipmap.joystick_bg, R.mipmap.joystick_thumb, 100, 1500, 100, 30, 80);
         player = new Player(joyStick);
+        Gauge gauge = new Gauge(10, Color.RED, Color.WHITE);
         Enemy.setTarget(player);
 
         Gun gun = new Gun(player, 50, 5, 0);
@@ -35,6 +38,8 @@ public class MainScene extends Scene {
 
         ScrollBackground bg = new ScrollBackground(R.mipmap.background, player);
         add(Layer.bg, bg);
+
+
 
         add(Layer.controller, new CollisionChecker(this));
         add(Layer.controller, new EnemyGenerator(this));
