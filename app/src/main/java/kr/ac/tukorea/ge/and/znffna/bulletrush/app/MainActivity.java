2ca79fda163b592590243a9645dce0b5baa6d10d
znffna <znffna@gmail.com>
2025-06-15 18:20:23 +0900

    Title에서도 노래를 실행

--- app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/app/MainActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/znffna/bulletrush/app/MainActivity.java
@@ -5,6 +5,7 @@ import static kr.ac.tukorea.ge.and.znffna.bulletrush.game.MainScene.Layer.ui;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,12 +16,17 @@ import kr.ac.tukorea.ge.and.znffna.bulletrush.R;
 import kr.ac.tukorea.ge.and.znffna.bulletrush.databinding.ActivityMainBinding;
 import kr.ac.tukorea.ge.and.znffna.bulletrush.game.Gun;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.Sound;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding ui;
     private int cookieIndex;
     private BitmapFactory.Options opts;
+
+    protected MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
         setWeaponIndex(0);
     }
-
-
 
     public void onBtnStartGame(View view) {
         Intent intent = new Intent(this, BulletRushActivity.class);
@@ -62,4 +66,32 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnNextWeapon(View view) {
         setWeaponIndex(cookieIndex + 1);
     }
+
+    @Override
+    protected void onStart() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.title_music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        mediaPlayer.start();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mediaPlayer.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        mediaPlayer.stop();
+        mediaPlayer = null;
+        super.onStop();
+    }
+
 }
